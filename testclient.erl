ddd81830f234499343cb136a419acf56fcbcef41
HansZimmer5000 <michael.weitnau@live.de>
2018-02-09 10:40:24 +0100

    changed function order

--- testclient.erl
+++ testclient.erl
@@ -5,18 +5,38 @@
 
 %TODO        frage_nach_neuer_nnr/0,
 %        erstelle_nachricht/2,
-%        erstelle_nachrichten_text/0,
+%TODO        erstelle_nachrichten_text/0,
 %TODO        pruefe_nnr_und_sende_nachricht/2,
 %        kalkuliere_neuen_intervall_sek/1,
 
 %TODO       frage_nach_neuer_nachricht/0,
-%TODO        empfangene_nachricht_ist_von_meinem_redakteur/2,
-%TODO        logge_empfangene_nachricht/2,
+%TODO       empfangene_nachricht_ist_von_meinem_redakteur/2,
+%TODO       logge_empfangene_nachricht/2,
 
 %        zufalls_boolean/0,
 %        element_ist_in_liste/2,
 %        nachricht_zu_text/1,
 %        neue_nnr_einfuegen/2
+
+frage_nach_neuer_nnr_1_test() ->
+    false.
+
+erstelle_nachricht_1_test() ->
+    NNR = 1,
+    TS = vsutil:now2string(erlang:timestamp()),
+    Text =  io_lib:format("hostname1, gruppe1, team1, ~s", [TS]),
+    TestNachricht = [NNR, Text, TS],
+    ResultNachricht = client:erstelle_nachricht(NNR, TS),
+    io:fwrite("\n"),
+    io:fwrite(TestNachricht),
+    io:fwrite("\n"),
+    io:fwrite(ResultNachricht),
+    TestNachricht == ResultNachricht.
+
+
+
+pruefe_nnr_und_sende_nachricht_1_test() ->
+    false.
 
 kalkuliere_neuen_intervall_sek_1_test() ->
     Result = client:kalkuliere_neuen_intervall_sek(2),
@@ -31,6 +51,20 @@ kalkuliere_neuen_intervall_sek_2_test() ->
 
 kalkuliere_neuen_intervall_sek_3_test() ->
     2.0 = client:kalkuliere_neuen_intervall_sek(a).
+
+
+
+
+frage_nach_neuer_nachricht_1_test() ->
+    false.
+
+empfangene_nachricht_ist_von_meinem_redakteur_1_test() ->
+    false.
+
+logge_empfangene_nachricht_1_test() ->
+    false.
+
+
 
 zufalls_boolean_1_test() ->
     case client:zufalls_boolean() of
@@ -47,6 +81,13 @@ element_ist_in_liste_2_test() ->
 
 element_ist_in_liste_3_test() ->
     false = client:element_ist_in_liste([],[a,b]).
+
+
+neue_nnr_einfuegen_1_test() ->
+    [1] = client:neue_nnr_einfuegen(1,[]).
+
+neue_nnr_einfuegen_2_test() ->
+    [1,2,3] = client:neue_nnr_einfuegen(1,[2,3]).
 
 nachricht_zu_text_1_test() ->
     TS = {1516, 47115, 874000},
@@ -66,23 +107,3 @@ nachricht_zu_text_2_test() ->
                 vsutil:now2string(TS)
                 ],
     "1, Test, 15.01 21:11:55,874|" = client:nachricht_zu_text(Nachricht).
-
-
-erstelle_nachricht_1_test() ->
-    NNR = 1,
-    TS = vsutil:now2string(erlang:timestamp()),
-    Text =  io_lib:format("hostname1, gruppe1, team1, ~s", [TS]),
-    TestNachricht = [NNR, Text, TS],
-    ResultNachricht = client:erstelle_nachricht(NNR, TS),
-    io:fwrite("\n"),
-    io:fwrite(TestNachricht),
-    io:fwrite("\n"),
-    io:fwrite(ResultNachricht),
-    TestNachricht == ResultNachricht.
-
-
-neue_nnr_einfuegen_1_test() ->
-    [1] = client:neue_nnr_einfuegen(1,[]).
-
-neue_nnr_einfuegen_2_test() ->
-    [1,2,3] = client:neue_nnr_einfuegen(1,[2,3]).
