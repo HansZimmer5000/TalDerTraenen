21502700678be18af4c3dd25c0dd913488ea3cb5
HansZimmer5000 <michael.weitnau@live.de>
2018-02-15 16:12:19 +0100

    More preperation and a lot more questions for A2

--- A2/vorbereitung.erl
+++ A2/vorbereitung.erl
@@ -1,7 +1,8 @@
 -module(vorbereitung).
 
 -export([
-    ggT/2
+    ggT/2,
+    ggTV/2
 ]).
 
 ggT(X, 0) -> X;
@@ -9,3 +10,10 @@ ggT(X, Y) ->
     NewX = Y,
     NewY = X rem Y,
     ggT(NewX, NewY).
+
+ggTV(X, Y) -> 
+    case Y < X of
+        true -> NewY = ((X - 1) rem Y) + 1;
+        false -> NewY = Y
+    end,
+    NewY.
