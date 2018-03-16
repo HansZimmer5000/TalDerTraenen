1bf256efa70f1dcb4515c1171ca4758ea7bf1987
HansZimmer5000 <michael.weitnau@live.de>
2018-03-16 15:52:56 +0100

    Added first Multicast test

--- A3/src/main/Main.java
+++ A3/src/main/Main.java
@@ -1,10 +1,54 @@
 package main;
 
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// join a Multicast group and send the group salutations
+		try {
+			String msg = "Hello";
+			String ip = "225.10.1.2";
+			int port = 6789; //Should be but is not working: 15001
+			
+			
+			
+			InetAddress group = InetAddress.getByName(ip);
+			MulticastSocket s = new MulticastSocket(port);
+			s.joinGroup(group);
+			
+			
+			
+			
+			
+			DatagramPacket hi = new DatagramPacket(msg.getBytes(), msg.length(), group, 6789);
+			s.send(hi);
+			
+			
+			
+			// get their responses!
+			byte[] buf = new byte[1000];
+			DatagramPacket recv = new DatagramPacket(buf, buf.length);
+			s.receive(recv);
+			System.out.println(new String(recv.getData(), StandardCharsets.UTF_8));
+			
+			
+			
+			
+			
+			// OK, I'm done talking - leave the group...
+			s.leaveGroup(group);
+			s.close();
+			}
+		
+		
+		catch(Exception e){
+			e.printStackTrace();
+		}
+			
 	}
 
 }
