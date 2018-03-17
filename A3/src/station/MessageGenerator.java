c43a2de1cde6200658cebc65f569a0b22a24a7fa
HansZimmer5000 <michael.weitnau@live.de>
2018-03-17 17:02:38 +0100

    Added new parameter to MessageGenerator:createMessage

--- A3/src/station/MessageGenerator.java
+++ A3/src/station/MessageGenerator.java
@@ -1,5 +1,23 @@
 package station;
 
 public class MessageGenerator {
-
+	
+	/*Nachrichtenaufbau:
+	    Gesamt 34 Byte
+	    - Byte 0        A oder B    Stationsklasse
+	    - Byte 1-24     -team-4711- Nutzdaten
+	    - Byte 25       4           reservierte Slotnummer für den nächsten Frame!
+	    - Byte 26-33    77394825    Zeit (gesendet) in ms seit 01.01.1970, 8-Byte Integer, Big Endian
+	*/
+	public static String convertMessageFromByte(byte[] messageInByte){
+		return "";
+	}
+	
+	public static byte[] convertMessageToByte(String message){
+		return message.getBytes();
+	}
+	
+	public static String createMessage(String stationType, String teamname, int slotNumber){
+		return stationType + teamname + String.valueOf(slotNumber);
+	}
 }
