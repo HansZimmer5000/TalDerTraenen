542fe471b9cc73e897f1206fe92b74138e3b2c6c
HansZimmer5000 <michael.weitnau@live.de>
2018-03-19 08:05:21 +0100

    added todo, new problem with current time detected

--- A3/src/station/UTCClock.java
+++ A3/src/station/UTCClock.java
@@ -11,9 +11,16 @@ public class UTCClock {
 	}
 	
 	public void adjust(ArrayList<String> messages){
+		String currentStationType;
+		long currentAdjustment;
 		
 		for (String currentMessage : messages){
-			
+			currentStationType = MessageHelper.getStationTypeF(currentMessage);
+			if(currentStationType == "A"){
+				//currentAdjustment = getAdjustment()
+				//TODO: Würden hier Zeit des Empfangs brauchen, dann wäre klar wie hoch der unterschied ist.
+				//Vom Empfang bis hier kann nämlich eine gewisse Zeit vergehen die das Ergebnsi verfälscht.
+			}
 		}
 	}
 }
