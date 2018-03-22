1057f090ceefc019e10be111a0c79713e9ce13d5
HansZimmer5000 <16050989+HansZimmer5000@users.noreply.github.com>
2018-03-22 23:45:31 +0100

    deleted  conversions, receiving lists now

--- A3/messagehelper.erl
+++ A3/messagehelper.erl
@@ -2,12 +2,8 @@
 
 
 -export([
-    convertMessagesFromByte/1,
-    convertMessagesFromByte/2,
-
     prepareForSending/2,
-    addSendTime/2,
-    convertMessageToByte/1
+    addSendTime/2
 ]).
 
 -define(SLOTNUMBERPOS, 25).
@@ -20,27 +16,7 @@
 %    - Byte 25       4           reservierte Slotnummer für den nächsten Frame!
 %    - Byte 26-33    77394825    Zeit (gesendet) in ms seit 01.01.1970, 8-Byte Integer, Big Endian
 
-convertMessagesFromByte(MessagesInByte) -> 
-    convertMessagesFromByte(MessagesInByte, []).
 
-convertMessagesFromByte([], ConvertedMessages) ->
-    ConvertedMessages;
-convertMessagesFromByte(MessagesInByte, ConvertedMessages) ->
-    [HeadMessage | RestMessages] = MessagesInByte,
-    NewConvertedMessage = convertMessageFromByte(HeadMessage),
-    NewConvertedMessages = [NewConvertedMessage | ConvertedMessages],
-    convertMessagesFromByte(RestMessages, NewConvertedMessages).
-
-convertMessageFromByte(MessageInByte) ->
-    StationTypeAndPayload = binary:bin_to_list(MessageInByte, ?SLOTNUMBERPOS - 1),
-    Slotnumber = binary:at(MessageInByte, ?SLOTNUMBERPOS),
-    SendTime = binary:bin_to_list(MessageInByte, ?SLOTNUMBERPOS + 1, ?SENDTIMELENGTH),
-    ConvertedMessage = lists:concat([
-        StationTypeAndPayload, 
-        Slotnumber,
-        SendTime
-    ]),
-    ConvertedMessage.
 
 prepareForSending(Message, SendTime) ->
     CompleteMessage = addSendTime(Message, SendTime),
@@ -50,22 +26,3 @@ prepareForSending(Message, SendTime) ->
 addSendTime(Message, SendTime) ->
     NewMessage = lists:concat([Message, SendTime]),
     NewMessage.
-
-convertMessageToByte(Message) ->
-    SlotnumberLength = length(Message) - 33,
-
-    StationTypeAndPayload = lists:sublist(Message, ?SLOTNUMBERPOS),
-    SlotnumberString = lists:sublist(
-                            Message, 
-                            ?SLOTNUMBERPOS + 1, 
-                            SlotnumberLength),
-    {Slotnumber, []} = string:to_integer(SlotnumberString),
-    SendTime = lists:sublist(Message, length(Message) - ?SENDTIMELENGTH + 1, ?SENDTIMELENGTH + 1),
-    ConvertedMessage = binary:list_to_bin([
-        StationTypeAndPayload, 
-        Slotnumber,
-        SendTime
-    ]),
-    %io:lib:format("~w", [String]).
-    io:fwrite("~p,~p,~p",[StationTypeAndPayload, Slotnumber, SendTime]),
-    ConvertedMessage.
--- A3/testmessagehelper.erl
+++ A3/testmessagehelper.erl
@@ -2,20 +2,8 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-%    convertMessagesFromByte/1,
-%    convertMessagesFromByte/2,
-
 %    prepareForSending/2,
 %    addSendTime/2,
-%    convertMessageToByte/1
-
-convertMessagesFromByte_1_test() -> 
-    io:fwrite("Not implemented yet"),
-    ?assert(false).
-
-convertMessagesFromByte_2_test() -> 
-    io:fwrite("Not implemented yet"),
-    ?assert(false).
 
 prepareForSending_1_test() -> 
     io:fwrite("Not implemented yet"),
@@ -34,33 +22,3 @@ addSendTime_2_test() ->
     SendTime = 77394825,
     IsResult = messagehelper:addSendTime(TestMessage, SendTime),
     ?assertEqual(ShouldResult, IsResult).
-
-convertMessageToByte_1_test() -> 
-    ShouldResult = <<"A-team-4711-123456789012-477394825">>,
-    TestMessage = "A-team-4711-123456789012-477394825",
-    IsResult = messagehelper:convertMessageToByte(TestMessage),
-    ?assertEqual(ShouldResult, IsResult).
-
-convertMessageToByte_2_test() -> 
-    ShouldResult = <<
-        65,
-        45,116,101,97,109,45,52,55,49,49,45,59,50,51,52,53,54,55,56,57,48,49,50,45,
-        25,
-        55,55,52,57,52,56,50,53
-    >>,
-    TestMessage = "A-team-4711-123456789012-25 77394825",
-    IsResult = messagehelper:convertMessageToByte(TestMessage),
-    io:fwrite("~p", [IsResult == ShouldResult]),
-    ?assertEqual(ShouldResult, IsResult).
-
-convertMessageToByte_3_test() -> 
-    ShouldResult = 34,
-    TestMessage = "A-team-4711-123456789012-2577394825",
-    IsResult = messagehelper:convertMessageToByte(TestMessage),
-    ?assertEqual(ShouldResult, byte_size(IsResult)).
-
-convertMessageToByte_4_test() -> 
-    ShouldResult = 34,
-    TestMessage = "A-team-4711-123456789012-477394825",
-    IsResult = messagehelper:convertMessageToByte(TestMessage),
-    ?assertEqual(ShouldResult, byte_size(IsResult)).
