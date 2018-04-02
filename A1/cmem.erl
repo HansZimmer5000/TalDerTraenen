15ca39519973326521712b245cb412f33f68f8eb
HansZimmer5000 <16050989+HansZimmer5000@users.noreply.github.com>
2018-04-02 12:03:49 +0200

    edited comments

--- A1/cmem.erl
+++ A1/cmem.erl
@@ -14,20 +14,25 @@
 
 -define(DEFAULT_NNR, 0).
 
-
+%------------------------------------------------------------------------------------------------------
+%										>>SCHNITTSTELLEN<<
+%------------------------------------------------------------------------------------------------------
+% Initialisiert eine CMEM ohne Tupel (leere Liste)
 initCMEM(ErinnerungsZeitSek, LogDatei) ->
     CMEM = {ErinnerungsZeitSek, []},
     logge_status("CMEM initalisiert", LogDatei),
     CMEM.
 
-
+% Setzt die neue NNr fuer einen bestimmten Client in dementsprechenden Tupel (setClientNNr)
+% loggt dies und gibt die neue CMEM zurueck.
 updateClient({ErinnerungsZeitSek, TupelListe}, ClientPid, NNr, LogDatei) ->
     NeueTupelListe = setClientNNr(TupelListe, ClientPid, NNr),
     logge_status(io_lib:format("~p update zu NNR: ~p", [ClientPid, NNr]), LogDatei),
     CMEM = {ErinnerungsZeitSek, NeueTupelListe},
     CMEM.
 
-
+% Setzt die neue NNr für einen bestimmten Client in dementsprechenden Tupel
+% Koennte man theoretisch noch zu Endrekursiv umschreiben.
 setClientNNr([], ClientPid, NNr) ->
     [{ClientPid, NNr, erlang:timestamp()}];
 setClientNNr(TupelListe, ClientPid, NNr) ->
@@ -40,6 +45,7 @@ setClientNNr(TupelListe, ClientPid, NNr) ->
     end,
     NeueTupelListe.
 
+% Holt die gespeicherte NNr von einem bestimmten Client.
 getClientNNr({_ErinnerungsZeitSek, []}, _ClientPid) -> 
     ?DEFAULT_NNR + 1;
 getClientNNr({ErinnerungsZeitSek, [KopfTupel | RestTupel]}, ClientPid) ->
@@ -50,22 +56,26 @@ getClientNNr({ErinnerungsZeitSek, [KopfTupel | RestTupel]}, ClientPid) ->
             getClientNNr({ErinnerungsZeitSek, RestTupel}, ClientPid)
     end.
 
+% Prueft ob der gesetzte Timestamp schon abgelaufen ist und gibt je nach dem die gespeicherte NNr zurueck.
 pruefe_ts_und_gib_nnr_zurueck(LetzterTS, ErinnerungsZeitSek, LetzteNNr) ->
     case ts_ist_abglaufen(LetzterTS, ErinnerungsZeitSek) of
         true ->  ?DEFAULT_NNR;
         false -> LetzteNNr
     end.
 
+% Prueft ob der Timestamp laenger her ist als es die Erinnerungszeit zulaesst.
 ts_ist_abglaufen(LetzterTS, ErinnerungsZeitSek) ->
     JetztTS = erlang:timestamp(),
     {_DiffMegaSec, DiffSec, _DiffMicroSec} = vsutil:diffTS(JetztTS, LetzterTS),
     DiffSec > ErinnerungsZeitSek.
 
-% Nicht in der Aufgabenstellung!
-% Dennoch vom Gegebenen Server verlangt.
+% Loescht die CMEM
 delCMEM(_CMEM) ->
     ok.
 
+%------------------------------------------------------------------------------------------------------
+%											>>LOGGING UND CONFIG<<
+%------------------------------------------------------------------------------------------------------
 
 logge_status(Inhalt, LogDatei) ->
     AktuelleZeit = vsutil:now2string(erlang:timestamp()),
