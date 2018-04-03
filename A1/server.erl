cebb457cdf76f1a8f87fc1d9116e797987372953
Mert Siginc <mert.siginc@haw-hamburg.de>
2018-04-03 15:22:31 +0200

    update server.erl add comments and timeouts

--- A1/server.erl
+++ A1/server.erl
@@ -24,6 +24,7 @@
 -define(HBQNAME, hole_wert_aus_config_mit_key(hbqname)).
 -define(HBQNODE, hole_wert_aus_config_mit_key(hbqnode)).
 -define(HBQ, {?HBQNAME, ?HBQNODE}).
+-define(TIMEOUT, 5).
 
 %------------------------------------------------------------------------------------------------------
 %											>>START / INIT<<
@@ -51,10 +52,10 @@ receive_loop(CMEM, NextNNR) ->
                                         receive_loop(NeueCMEM, NextNNR);
         {dropmessage, Nachricht} ->     dropmessage_abfertigen(?HBQ, Nachricht),
                                         receive_loop(CMEM, NextNNR);
-        {AbsenderPid, getmsgid} ->  NeueNextNNR = getmsgid_abfertigen(AbsenderPid, NextNNR),
-                                    receive_loop(CMEM, NeueNextNNR);
-        UnbekanntesKommando ->  logge_status(io_lib:format("Bekam unbekanntes Kommando ~p", [UnbekanntesKommando])),
-                                receive_loop(CMEM, NextNNR)
+        {AbsenderPid, getmsgid} ->  	NeueNextNNR = getmsgid_abfertigen(AbsenderPid, NextNNR),
+										receive_loop(CMEM, NeueNextNNR);
+        UnbekanntesKommando ->  		logge_status(io_lib:format("Bekam unbekanntes Kommando ~p", [UnbekanntesKommando])),
+										receive_loop(CMEM, NextNNR)
         
         after timer:seconds(?LATENZ_SEK) -> runterfahren(CMEM)
     end.
@@ -63,6 +64,8 @@ receive_loop(CMEM, NextNNR) ->
 %------------------------------------------------------------------------------------------------------
 %                           >>EIGENTLICHE FUNKTIONEN<<
 %------------------------------------------------------------------------------------------------------
+% Diese Funktion wickelt die Leseanfrage des Clients ab.
+% Es wird die aktualisierte CMEM zurückgegeben.
 getmessages_abfertigen(HBQPid, CMEM, LeserPid) -> 
     ZuSendendeNNr = hole_naechste_nnr_fur_leser(CMEM, LeserPid),
     GesendeteNNr = sendeNNr(HBQPid, ZuSendendeNNr, LeserPid),
@@ -70,44 +73,55 @@ getmessages_abfertigen(HBQPid, CMEM, LeserPid) ->
     NeueCMEM = update_gesendete_nnr_fur_leser(CMEM, LeserPid, GesendeteNNr),
     NeueCMEM.
 
+% Diese Funktion hollt aus der CMEM die naechste NNR für den jeweiligen Client. 
 hole_naechste_nnr_fur_leser(CMEM, LeserPid) ->
     cmem:getClientNNr(CMEM, LeserPid).
 
+% Diese Funktionen übergibt der CMEM die letzte gesendete NNR zum jeweiigen Client
+% um diese zu updaten.
 update_gesendete_nnr_fur_leser(CMEM, LeserPid, LetzteGesendeteNNr) ->
     NeueCMEM = cmem:updateClient(CMEM, LeserPid, LetzteGesendeteNNr, ?CMEM_LOG_DATEI_NAME),
     NeueCMEM.
 
+% Diese Funktion beauftragt die HBQ die NNR an den jeweiligen Client zu senden und gibt bei erfolg die gesendeteNNR zurück.
+% Falls keine Antwort vom HBQ erhalten wurde, wird die letze NNR zurückgegeben.
 sendeNNr(HBQPid, ZuSendendeNNr, LeserPid) ->
     HBQPid ! {self(), {request, deliverMSG, ZuSendendeNNr, LeserPid}},
     receive
-        {reply, GesendeteNNr} -> GesendeteNNr
+        {reply, GesendeteNNr} -> 
+			GesendeteNNr
+		after timer:seconds(?TIMEOUT) -> 
+            logge_status(io_lib:format("Die HBQ konnte nicht beauftragt werden, die Nachricht mit der der Nummer ~p an den Client zu schicken.", [ZuSendendeNNr])),
+			ZuSendendeNNr - 1 
     end.
 
-
+% Verschickt die Nachricht vom Client an die HBQ, das Ergebnis wird geloggt.
 dropmessage_abfertigen(HBQPid, Nachricht) ->
     HBQPid ! {self(), {request, pushHBQ, Nachricht}},
     [NNr | _Rest] = Nachricht,
     receive
         {reply, ok} ->
             logge_status(io_lib:format("Nachricht mit Nummer ~p wurde an HBQ geschickt", [NNr]))
-        after timer:seconds(5) -> 
+        after timer:seconds(?TIMEOUT) -> 
             logge_status(io_lib:format("Nachricht mit Nummer ~p wurde NICHT erfolgreich an HBQ geschickt", [NNr]))
     end.
 
-
+% Hierüber wird eine eindeutige NNR an den Client verschickt.
 getmsgid_abfertigen(AbsenderPid, LetzteNNR) -> 
     AbsenderPid ! {nid, LetzteNNR},
     logge_status(io_lib:format("NNr ~p an ~p gesendet", [LetzteNNR, AbsenderPid])),
     LetzteNNR + 1.
 
 
-
+%Hierrüber werden die HBQ und CMEM gelöscht.
 runterfahren(CMEM) ->
     logge_status("Server wird heruntergefahren"),
     ?HBQ ! {self(), {request, dellHBQ}},
     receive
-        {reply, ok} -> logge_status("HBQ erfolgreich heruntergefahren")
-        after timer:seconds(5) -> logge_status("HBQ nicht erfolgreich heruntergefahren")
+        {reply, ok} -> 
+			logge_status("HBQ erfolgreich heruntergefahren")
+        after timer:seconds(?TIMEOUT) -> 
+			logge_status("HBQ nicht erfolgreich heruntergefahren")
     end,
     ok = cmem:delCMEM(CMEM),
     true = unregister(?SERVERNAME),
