b3e2e45ea5ed9e0376c1659e65ce91e2802db3f6
HansZimmer5000 <16050989+HansZimmer5000@users.noreply.github.com>
2018-04-10 12:57:24 +0200

    starter ok now

--- A2/teststarter.erl
+++ A2/teststarter.erl
@@ -9,8 +9,9 @@
 %    create_ggtproname/2
 
 go_1_test() ->
-    GGTProName1 = 'ggt-1121',
+    GGTProName1 = 'ggt-1261',
     ThisPid = self(),
+    register(nameservice, ThisPid),
     StarterPid = spawn(
                     fun() ->
                         starter:go(1, ThisPid)
@@ -22,14 +23,17 @@ go_1_test() ->
     timer:sleep(timer:seconds(1)),
     Result = whereis(GGTProName1),
     io:fwrite("Ergebnis: ~p, ggf. Sleepzeit anpassen bei langsameren PCs", [Result]),
-    ?assert(is_pid(Result)).
+    ?assert(is_pid(Result)),
+    unregister(nameservice).
 
 
 start_all_ggtprozesse_1_test() ->
-    GGTProName1 = 'ggt-2121',
+    GGTProName1 = 'ggt-2261',
+    register(nameservice, self()),
     starter:start_all_ggtprozesse(2, {empty, empty, empty, 1}),
     Result = whereis(GGTProName1),
-    ?assert(is_pid(Result)).
+    ?assert(is_pid(Result)),
+    unregister(nameservice).
 
 create_ggtproname_1_test() -> 
-    ?assertEqual('ggt-1123', starter:create_ggtproname(1, 3)).
+    ?assertEqual('ggt-1263', starter:create_ggtproname(1, 3)).
