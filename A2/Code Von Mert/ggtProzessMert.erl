e29719047d1b1a039e42d6657cdf74a06fdc2af3
Mert Siginc <mert.siginc@haw-hamburg.de>
2018-04-18 00:20:15 +0200

    ggtProzessMert.erl fixed

--- A2/Code Von Mert/ggtProzessMert.erl
+++ A2/Code Von Mert/ggtProzessMert.erl
@@ -1,16 +1,16 @@
--module(client).
+-module(ggtProzessMert).
 
 % API
 -export([
-        start/4,
+        start/5
         ]).
 
 % KONSTANTEN
 -define(CONFIG_FILENAME, 	"ggt.cfg").
 -define(PRAKTIKUMSGRUPPE, 	hole_wert_aus_config_mit_key(praktikumsgruppe)).
 -define(TEAMNUMMER, 		hole_wert_aus_config_mit_key(teamnummer)).
--define(NSNODE,				hole_wert_aus_config_mit_key(NSnode)).
--define(KO, 			hole_wert_aus_config_mit_key(KO)).
+-define(NSNODE,				hole_wert_aus_config_mit_key(nameservicenode)).
+-define(KO, 				hole_wert_aus_config_mit_key(koordinatorname)).
 
 %------------------------------------------------------------------------------------------------------
 %										>>START / INIT<<
@@ -23,11 +23,11 @@ start(VZ, TZ, StarterNummer, ProzessNummer, Q) ->
 	register(Name, SelfPid),	
 	logge_status(io_lib:format("Prozess mit dem Namen ~p gestartet", [Name]), LogDatei).
 	
-init(VZ, TZ, Name, Q LogDatei) ->	
-	KO = getPid(?KOORDINATOR),
-	NS = getPid(?NSNODE),
+init(VZ, TZ, Name, Q, LogDatei) ->	
+	KO = getPid(?KO, LogDatei),
+	NS = getPid(?NSNODE, LogDatei),
 	
-	NS ! {this(),{rebind, node()}},
+	NS ! {self(),{rebind, node()}},
 	receive
 		ok ->		
 			logge_status(io_lib:format("Prozess ~p wurde in den NS eingeschrieben.", [Name]), LogDatei)
@@ -45,7 +45,7 @@ init(VZ, TZ, Name, Q LogDatei) ->
 	
 	receive
         {setpm, MiNeu} -> 	
-			Mi = MiNeu
+			Mi = MiNeu,
 			logge_status(io_lib:format("Mi wurde auf ~p gesetzt.", [MiNeu]), LogDatei)
 	end,
 	
@@ -56,15 +56,15 @@ lookup_name_at_ns(Name, NsPid) ->
     NsPid ! {self(), {lookup, Name}},
     receive
         {pin, Pid} -> Pid;
-        not_found -> not_found %TODO
+        not_found -> not_found
     end.
 	
-getPid(Node) -> 
+getPid(Node, LogDatei) -> 
     net_adm:ping(Node),
     timer:sleep(timer:seconds(2)),
     case global:whereis_name(Node) of
         undefined ->            
-			logge_status(io_lib:format("~p nicht gefunden, ggt wird beendet.", [Node]), LogDatei).
+			logge_status(io_lib:format("~p nicht gefunden, ggt wird beendet.", [Node]), LogDatei),
             timer:sleep(timer:seconds(5)),
             exit(kill);
         Pid -> Pid
@@ -73,7 +73,7 @@ getPid(Node) ->
 %								>>LOOPS<<
 %------------------------------------------------------------------------------------------------------
 prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei) -> 
-	prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei, 0, {false, false})
+	prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei, 0, {false, false}).
 
 prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei, VoteCount, VoteCfg) -> 
 	{TermFlag, VoteFlag} = VoteCfg,
@@ -83,7 +83,7 @@ prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei, VoteCount, VoteCfg
 			prozess_loop(VZ, TZ, NS, KO, Name, MiNeu, Nachbarn, Q, LogDatei, 0, {false, false});
 		 
 		{sendy, Y} ->
-			MiNeu = calcGGT(Mi, Y, Nachbarn, KO, Name),
+			MiNeu = calcGGT(Mi, Y, Nachbarn, KO, Name, LogDatei),
 			prozess_loop(VZ, TZ, NS, KO, Name, MiNeu, Nachbarn, Q, LogDatei, VoteCount, {TermFlag, false});
 			
 		{From, tellmi} ->
@@ -97,30 +97,31 @@ prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei, VoteCount, VoteCfg
 			prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei, VoteCount, VoteCfg);
 		
 		{voteYes, Name} ->
-			{NewTerm, NewVoteCount, BriefFlag} = handleIncVote({false, false}, Q, VoteCount),			
+			{NewTerm, NewVoteCount, BriefFlag} = handleIncVote(TermFlag, Q, VoteCount),			
 			logge_status(io_lib:format("Vote von ~p erhalten.", [Name]), LogDatei),
 			case BriefFlag of
 				true ->
-					briefTerm(KO, Name, Mi)					
+					briefTerm(KO, Name, Mi, LogDatei)					
 			end,			
-			prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei, NewVoteCount, {NewTerm, VoteRdyFlag})
+			prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei, NewVoteCount, {NewTerm, VoteFlag});
 		
 		{From, {vote, Initiator}} ->   
-			case TermFlag of
+			case TermFlag of %TODO TZ / 2
 				true -> 
-					From ! {voteYes, Name}
+					From ! {voteYes, Name},
+					logge_status(io_lib:format("Vote Anfrage von ~p erhalten und mit Ja gestimmt.", [Initiator]), LogDatei)
 			end,			
 			prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei, VoteCount, VoteCfg);
 			
 		kill -> 
-			kill(Name, NS);
+			kill(Name, NS, LogDatei)
      
         after timer:seconds(TZ) ->  
 			case VoteFlag of
 				false ->
 					case TermFlag of
 						false ->
-							NS ! {self(), {multicast, vote, Name},					
+							NS ! {self(), {multicast, vote, Name}},					
 							prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei, 0, {TermFlag, true})
 					end
 			end		
@@ -131,24 +132,24 @@ prozess_loop(VZ, TZ, NS, KO, Name, Mi, Nachbarn, Q, LogDatei, VoteCount, VoteCfg
 %								>>EIGENTLICHE FUNKTIONEN<<
 %------------------------------------------------------------------------------------------------------
 
-calcGGT(Mi, Y, {LeftN, RightN}, KO, Name) -> 
+calcGGT(Mi, Y, {LeftN, RightN}, KO, Name, LogDatei) -> 
     case Mi > Y of
         true -> 
 			NewMi = ((Mi - 1) rem Y) + 1,			
 			logge_status(io_lib:format("Neues Mi: ~p wurde Berechnet der KOORDINATOR und die Nachbarn wurde bescheid gesagt.", [NewMi]), LogDatei),
 			LeftN !  {sendy, NewMi},
 			RightN ! {sendy, NewMi},
-			KoPid !  {briefmi, {Name, NewMi, vsutil:now2string(erlang:timestamp())}};
+			KO !  {briefmi, {Name, NewMi, vsutil:now2string(erlang:timestamp())}};
         false -> 
 			NewMi = Mi
     end,
     NewMi.
 	
-handleIncVote(Term, Q, VoteCount)
+handleIncVote(Term, Q, VoteCount) ->
 	NewVoteCount = VoteCount + 1,	
 	case Term of
 		false ->
-			case (NewVoteCount >= Q) ->
+			case (NewVoteCount >= Q) of
 				true ->
 					Ret ={true, NewVoteCount, true};
 				false ->
@@ -159,18 +160,18 @@ handleIncVote(Term, Q, VoteCount)
 	end,
 	Ret.
 					
-briefTerm(KO, Name, Mi) ->
+briefTerm(KO, Name, Mi, LogDatei) ->
     KO ! {self(), briefterm, {Name, Mi, vsutil:now2string(erlang:timestamp())}},
 	logge_status("Genug Votes erhalten und Terminiert.", LogDatei).
 	
-kill(Name, NsPid) ->
-    NsPid ! {self(), {unbind, GGTProName}},
+kill(Name, NsPid, LogDatei) ->
+    NsPid ! {self(), {unbind, Name}},
     receive
         ok -> 
 			logge_status("Aus dem NS entfernt.", LogDatei)
     end,
 	unregister(Name),    
-	ogge_status(io_lib:format("~p ist heruntergefahren.", [Name]), LogDatei),
+	logge_status(io_lib:format("~p ist heruntergefahren.", [Name]), LogDatei),
 	exit(kill).
 	
 
