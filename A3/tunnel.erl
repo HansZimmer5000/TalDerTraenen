489576a1be830def47f2d4b5a190ae751aeb17de
HansZimmer5000 <16050989+HansZimmer5000@users.noreply.github.com>
2018-04-27 14:01:47 +0200

    added test, giving up, doesn't work as I want on windows

--- A3/tunnel.erl
+++ A3/tunnel.erl
@@ -2,9 +2,7 @@
 
 -export(
     [
-        test/0,
-        send/0,
-        recv/0
+        test/0
     ]).
 
 -define(LIFETIME, 10000).
@@ -16,49 +14,54 @@
 -define(HOMEPORT, 15006).
 
 test() ->
-    spawn(fun() -> recv() end),
+    test1(), 
+    io:fwrite("\n\nTest2\n\n"),
+    test2().
+
+test1() ->
+    spawn(fun() -> recv({0,0,0,0}) end),
     timer:sleep(timer:seconds(1)), 
-    %spawn(fun() -> recv() end),
+    spawn(fun() -> send() end).
+
+test2() ->
+    spawn(fun() -> recv({0,0,0,0}) end),
     timer:sleep(timer:seconds(1)), 
-    io:fwrite("\n\n"), spawn(fun() -> send() end).
+    spawn(fun() -> recv({10,0,112,251}) end),
+    timer:sleep(timer:seconds(1)), 
+    spawn(fun() -> send() end),
+    timer:sleep(timer:seconds(1)),
+    spawn(fun() -> send() end),
+    nothing.
 
 send() ->
-    io:fwrite("Starting Sending"),
     MultiCastAddr = ?HOMEMCADDR,
     Port = ?HOMEPORT,
     Source = {10,0,112,251},
 
     Options = [
-        binary, inet, 
-        {active,false}, {multicast_if, MultiCastAddr},
-        {reuseaddr,true}, {add_membership, {MultiCastAddr, Source}},
+        binary, inet, -
+        {active, false}, %{multicast_if, MultiCastAddr},
+        {add_membership, {MultiCastAddr, Source}},
         {ip, Source}, {multicast_ttl, 1}, {multicast_loop, true}
     ],
     {ok, Socket} = gen_udp:open(Port, Options),
 
-    io:fwrite("\nSending now"),
     ok = gen_udp:send(Socket, MultiCastAddr, Port, <<"TextNachricht">>),
 
     timer:apply_after(?LIFETIME, gen_udp, close, [Socket]).
 
-recv() ->
-    io:fwrite("Starting Receiving"),
+recv(Addr) ->
     MultiCastAddr = ?HOMEMCADDR,
     Port = ?HOMEPORT,
-    Addr = {0,0,0,0},
 
     Options = [
         binary, inet,	
-        {active, false}, 
-        {reuseaddr, true}, 
+        {active, true},
         {multicast_ttl, 1}, 
         {multicast_loop, true}, {add_membership, {MultiCastAddr, Addr}}
     ],
     {ok, Socket} = gen_udp:open(Port, Options),
     gen_udp:controlling_process(Socket, self()),
-
-    io:fwrite("\nReceiving now"),
-    {ok, {Address, Port, Packet}} = gen_udp:recv(Socket, 0),
-    io:fwrite("Got: ~p, ~p, ~p", [Address, Port, Packet]),
+    receive Any -> io:fwrite("~p", [Any]) end,
 
     timer:apply_after(?LIFETIME, gen_udp, close, [Socket]).
