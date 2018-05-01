527b952c956eea010f72774c7dbb446148da77cc
HaPe <michael.mueller2@haw-hamburg.de>
2018-05-01 23:39:40 +0200

    first somekind of gen_udp prototype, possible to run on mac

--- A3/tunnel.erl
+++ A3/tunnel.erl
@@ -2,43 +2,59 @@
 
 -export(
     [
-        test/0
+        test/0,
+        recv/1,
+        send/0
     ]).
 
 -define(LIFETIME, 10000).
 
--define(PRAKTIKUMMCADDR, {225,0,10,1}).
+-define(PRAKTIKUMMCADDR, {224,0,0,251}).%{225,0,10,1}).
 -define(PRAKTIKUMPORT, 15006).
 
 -define(HOMEMCADDR, {239,1,1,3}).
 -define(HOMEPORT, 15006).
 
 test() ->
-    test1(). %,io:fwrite("\n\nTest2\n\n"),test2().
-
-test1() ->
-    spawn(fun() -> recv({0,0,0,0}) end),
-    timer:sleep(timer:seconds(1)), 
-    spawn(fun() -> send() end).
-
-test2() ->
-    spawn(fun() -> recv({0,0,0,0}) end),
-    timer:sleep(timer:seconds(1)), 
-    spawn(fun() -> recv({10,0,112,251}) end),
-    timer:sleep(timer:seconds(1)), 
-    spawn(fun() -> send() end),
+    spawn(fun() -> receive0() end),
+    spawn(fun() -> receive0() end),
     timer:sleep(timer:seconds(1)),
-    spawn(fun() -> send() end),
-    nothing.
+    %spawn(fun() -> send0() end),
+    spawn(fun() -> send0() end).
 
+receive0() ->
+    {ok, Socket} = gen_udp:open(?PRAKTIKUMPORT, [
+        {mode, binary},
+        {reuseaddr, true},
+        {ip, {224, 0, 0, 251}},
+        {multicast_ttl, 4},
+        {multicast_loop, true},
+        {broadcast, true},
+        {add_membership, {{224, 0, 0, 251}, {0, 0, 0, 0}}},
+        {active, false}]), %once
+
+    gen_udp:controlling_process(Socket, self()),
+    io:fwrite("receiving"),
+
+    % Egal welche Abholung -> Die Nachrichten werden wohl nicht gehalten sondern nur neue werden gesichtet.
+    % TODO: mal mit Socket erstellung und weitergebung machen.
+    %receive {udp, _Socket0, _Ip0, _Port0, Message} -> io:fwrite("~p", [Message]) end,
+    io:fwrite("~p", [gen_udp:recv(Socket, 0)]),
+    receive0().
+
+send0() ->
+    {ok, Socket} = gen_udp:open(0, [binary]),
+    gen_udp:send(Socket, {224, 0, 0, 251}, ?PRAKTIKUMPORT, <<"hello world">>),
+    io:fwrite("sended").
+            
 send() ->
     MultiCastAddr = ?HOMEMCADDR,
     Port = ?HOMEPORT,
     Source = {10,0,112,251},
 
     Options = [
-        binary, inet, -
-        {active, false}, %{multicast_if, MultiCastAddr},
+        binary, inet,
+        {active, true}, %{multicast_if, MultiCastAddr},
         {add_membership, {MultiCastAddr, Source}},
         {ip, Source}, {multicast_ttl, 1}, {multicast_loop, true}
     ],
