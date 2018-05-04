f203a3f96cf3919653d0328cfcc279f89331eca0
HansZimmer5000 <16050989+HansZimmer5000@users.noreply.github.com>
2018-05-04 14:20:44 +0200

    added testing for receiver

--- A3/receiver.erl
+++ A3/receiver.erl
@@ -1,6 +1,12 @@
 -module(receiver).
 
--export([start/1]).
+-export([
+    start/1,
+    start/2,
+    
+    loop/1,
+    listen_to_slot/1
+    ]).
 
 -define(NSNODE, hole_wert_aus_config_mit_key(node)).
 -define(NSNAME, nameservice).
@@ -8,36 +14,44 @@
 -define(SLOTLENGTHMS, 20).
 
 start(CorePid) ->
+    start(CorePid, {?NSNAME, ?NSNODE}).
+
+start(CorePid, NsPid) ->
     Pid = spawn(fun() -> loop(CorePid) end),
-    {?NSNAME, ?NSNODE} ! {enlist, Pid},
+    NsPid ! {enlist, Pid},
     Pid.
 
 loop(CorePid) ->
     receive
         {udp, _Socket0, _Ip0, _Port0, Message} ->
             io:fwrite(io_lib:format("Missed Message: ~p in loop", [Message]));
-        listen_to_slot -> 
-            timer:send_after(?SLOTLENGTHMS, self(), stop_listening),
-            listen_to_slot(CorePid, [], []);
+        listentoslot -> 
+            listen_to_slot(CorePid);
         Any -> 
             io:fwrite(io_lib:format("Got: ~p in loop", [Any]))
     end,
     loop(CorePid).
 
-listen_to_slot(CorePid, SlotMessages, ReceivedTimes) ->
+listen_to_slot(CorePid) ->
+    timer:send_after(?SLOTLENGTHMS, self(), stop_listening),
+    listen(CorePid, [], []).
+
+listen(CorePid, SlotMessages, ReceivedTimes) ->
     receive
         {udp, _Socket0, _Ip0, _Port0, Message} -> 
             NewSlotMessages = [Message | SlotMessages],
             NewReceivedTimes = [erlang:timestamp() | ReceivedTimes],
-            listen_to_slot(CorePid, NewSlotMessages, NewReceivedTimes);
+            listen(CorePid, NewSlotMessages, NewReceivedTimes);
         stop_listening ->
-            CorePid ! {slotMessages, SlotMessages, ReceivedTimes},
+            CorePid ! {slotmessages, SlotMessages, ReceivedTimes},
             loop(CorePid);
         Any -> 
             io:fwrite(io_lib:format("Got: ~p in listen_to_slot", [Any])),
-            listen_to_slot(CorePid, SlotMessages, ReceivedTimes)
+            listen(CorePid, SlotMessages, ReceivedTimes)
     end.
 
+
+%-------------------------------------------------------------------
 
 hole_wert_aus_config_mit_key(Key) ->
     {ok, ConfigListe} = file:consult('nameservice.cfg'),
